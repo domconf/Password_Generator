a8c3240ba559b11ab73ac4e4b1fb914ae187a1ca
Dominick Confessore <domconfessore@gmail.com>
2023-03-13 13:51:02 -0400

    Added notes to code

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 
 // Assignment code here
+
+// Added character type variables
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
+
+// Added generatePassword function
 var generatePassword = function () {
   var newPassword = ""
 
@@ -17,6 +21,8 @@ var generatePassword = function () {
   } else if (passLength < 8 || passLength > 128) {
     alert("Character length does not meet criteria.");
 
+
+    //Added password length requirements
   } else if (passLength >= 8 || passLength <= 128) {
     var getLowerCase = confirm("Include lowercase?");
     var getUpperCase = confirm("Include uppercase?");
