da921a26d38785cc2442e954a44cb5c395d21118
Dominick Confessore <domconfessore@gmail.com>
2023-03-13 13:39:43 -0400

    passLength function created

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
+var generatePassword = function () {
+  var newPassword = ""
+
+  var passLength = prompt("Choose a password character length between 8 and 128 characters.");
+
+  if (!passLength) {
+    alert("Input value.");
+  } else if (passLength < 8 || passLength > 128) {
+    alert("Character length does not meet criteria.")
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
