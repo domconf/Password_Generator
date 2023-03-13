0972e192fdf0088fe9ce3e9aa6d0fc40a6cc838d
Dominick Confessore <domconfessore@gmail.com>
2023-03-13 14:11:40 -0400

    Changed var to const in character type

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // Assignment code here
 
 // Added character type variables
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 
 // Added generatePassword function
@@ -50,6 +50,7 @@ var generatePassword = function () {
         newPassword = newPassword + special[index]
       }
     }
+    return newPassword
   }
 }
 
